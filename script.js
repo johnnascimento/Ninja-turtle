74b1e48b8d872552a3de0cd770085c252fa9e3b3
johnnascimento <jlenonns@gmail.com>
2019-08-14 13:37:43 -0300

    adding prototype

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ console.log("Ninja turtle script");
 console.log("*******************************");
 
 // Helpers
+// ----------------------------------
 const d = document;
 const b = d.body || d.bodyElement;
 const docQuery = (elem) => {
@@ -14,6 +15,8 @@ const cl = (text, elem) => {
   return console.log('%c '+text, 'font-size: 16px; font-weight: 700; color: coral;', elem);
 };
 
+// Class
+//----------------------------------
 const Turtle = function(name)  {
   this.name = name;
   this.sayHi = () => {
@@ -22,15 +25,38 @@ const Turtle = function(name)  {
 };
 
 Turtle.prototype.weapon = function(weapon, number) {
-  this.weapon = (number <= 2) ? number + ' ' + weapon + ' weapons': number + ' ' + weapon + ' weapon' ;
+  this.weapon = (number <= 2) ? number + ' ' + weapon + ' weapon': number + ' ' + weapon + ' weapons' ;
   return `${name} has ${weapon}`;
 };
 
+Turtle.prototype.attack = function() {
+  return `${this.name} hits with his ${this.weapon}.`;
+};
+
+Turtle.prototype.move = function(up, right, down, left) {
+  if(up !== undefined || up !== null) {
+    return `${this.name} moved ${up}.`;
+  } 
+  
+  if(right !== undefined || right !== null) {
+    return `${this.name} moved ${right}.`;
+  }
+  
+  if(down !== undefined || down !== null) {
+    return `${this.name} moved ${down}.`;
+  } 
+  
+  if(left !== undefined || left !== null) {
+    return `${this.name} moved ${left}.`;
+  }
+};
+
+// Classes in use
+// ------------------------------------
 let raph = new Turtle('Raphael');
 raph.sayHi();
-console.log(raph.weapon);
-console.log(`${raph.name} ${raph.weapon('Baston', 1)}`);
-
+cl(raph.weapon);
+cl(`${raph.name} ${raph.weapon('Baston', 1)}`);
 cl(raph.weapon);
 cl(raph.hasOwnProperty('name'));
 cl(raph.hasOwnProperty('sayHi'));
@@ -41,3 +67,4 @@ cl(don.weapon('sword',1));
 don.sayHi();
 
 cl(raph.constructor.prototype);
+cl(raph.attack());
