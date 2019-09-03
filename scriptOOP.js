683b34b45afd9e109fa385938ed5ae2552006dab
johnnascimento <jlenonns@gmail.com>
2019-09-03 09:00:06 -0300

    initialised another script to tackle oop concept

---
+++ scriptOOP.js
@@ -0,0 +1,16 @@
+cl('************************');
+cl('OOP studies');
+cl('************************');
+
+const human = {
+  head: 1,
+  dorso: 1,
+  arms: 2,
+  legs: 2,
+  
+  walk: function() {
+    cl('Walking');
+  }
+};
+
+cl(human);
