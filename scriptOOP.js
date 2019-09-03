eef6dad59f14ace676b0a8639894cdcd2b5d1246
johnnascimento <jlenonns@gmail.com>
2019-09-03 09:07:47 -0300

    super class and property descriptors

--- scriptOOP.js
+++ scriptOOP.js
@@ -2,7 +2,7 @@ cl('************************');
 cl('OOP studies');
 cl('************************');
 
-const human = {
+const Human = {
   head: 1,
   dorso: 1,
   arms: 2,
@@ -13,4 +13,17 @@ const human = {
   }
 };
 
-cl(human);
+const Heroe = Object.create(Human);
+
+cl(Human);
+cl(Human);
+
+petter = Object.create(Human);
+petter.name = 'Petter Parker';
+petter.age = 20;
+petter.job = 'Photographer';
+cl(petter.name);
+
+gwen = Object.create(Human, { name: { value: 'Gwen Stacy', enumerable: true}, age: { value: 20, enumerable: true}, job: { value: 'Scientist', enumerable: true}});
+cl(gwen.name);
+
